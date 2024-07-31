842be3de0f977487120af59c1b3c48efd7256d64
Magdalena Grzesik <magdalena.grzersikk@gmail.com>
2024-07-31 21:13:10 +0200

    Add task 03

--- 03/app.js
+++ 03/app.js
@@ -1,14 +1,31 @@
-console.log('DOM');
+console.log("DOM");
 
 const buttonSettings = {
-    attr: {
-        className: 'btn',
-        title: 'super button'
-    },
-    css: {
-        border: '1px solid #336699',
-        padding: '5px 20px',
-        color: '#444'
-    },
-    text: 'Click me!',
+  attr: {
+    className: "btn",
+    title: "super button",
+  },
+  css: {
+    border: "1px solid #336699",
+    padding: "5px 20px",
+    color: "#444",
+  },
+  text: "Click me!",
+};
+const buttonElement = document.createElement("button");
+
+const parentElement = document.querySelector(".parent-for-button");
+
+parentElement.appendChild(buttonElement);
+
+for (const key in buttonSettings) {
+  for (const key2 in buttonSettings[key]) {
+    if (key === "attr") {
+      buttonElement.setAttribute(key2, buttonSettings[key][key2]);
+    } else if (key === "css") {
+      buttonElement.style.setProperty(key2, buttonSettings[key][key2]);
+    } else if (key === "text") {
+      buttonElement.innerText = buttonSettings[key];
+    }
+  }
 }
