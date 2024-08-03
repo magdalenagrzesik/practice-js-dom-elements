6508efac4122e045fe9d43bd46c8c3ed9fa57120
Magdalena Grzesik <magdalena.grzersikk@gmail.com>
2024-08-03 16:57:44 +0200

    Add task 05

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,36 @@
-console.log('DOM');
+console.log("DOM");
 
-const curr = document.querySelector('.js-curr');
+const curr = document.querySelector(".js-curr");
+
+//1
+const button = document.createElement("button");
+const currParent = curr.parentElement;
+button.textContent = "usuń z koszyka";
+currParent.appendChild(button);
+
+//2
+const currSiblings = currParent.children;
+for (let i = 0; i < currSiblings.length; i++) {
+  currSiblings[i].setAttribute("class", "siblings");
+}
+
+//3
+currParent.nextElementSibling.setAttribute("title", "nextElementSibling");
+
+//4
+const articles = currParent.parentElement.children;
+const lastArticle = articles[articles.length - 1];
+const paragraph = document.createElement("p");
+lastArticle.insertBefore(paragraph,lastArticle.lastElementChild)
+
+console.dir(articles);
+
+//5
+const article = document.createElement("article");
+const h1 = document.createElement("h1");
+const p = document.createElement("p");
+const button2 = document.createElement("button");
+article.appendChild(h1)
+article.appendChild(p)
+article.appendChild(button2)
+articles[0].parentElement.insertBefore(article, articles[0])
